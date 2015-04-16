eca66ee21e4c2b61de774f4e7af3ed86521b6f1d
Luis <lgaa2006@gmail.com>
2015-04-16 12:42:15 -0300

    Removing unnecessary constructor

--- src/main/java/br/com/caelum/agiletickets/domain/precos/AcrescimoParaEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/AcrescimoParaEspetaculo.java
@@ -2,10 +2,6 @@ package br.com.caelum.agiletickets.domain.precos;
 
 public class AcrescimoParaEspetaculo {
 
-	public AcrescimoParaEspetaculo() {
-		super();
-	}
-
 	protected double porcentagemDeIngressosFaltantes(double totalDeIngressos, double ingressosRestantes) {
 		return ingressosRestantes / totalDeIngressos;
 	}
