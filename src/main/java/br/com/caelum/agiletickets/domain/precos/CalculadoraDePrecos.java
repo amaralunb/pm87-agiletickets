62bc5beb469b95cacf2432a6cd1dfd8107166b8e
Luis <lgaa2006@gmail.com>
2015-04-16 11:44:55 -0300

    Refactoring: criação de variáveis locais

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -12,26 +12,28 @@ import static br.com.caelum.agiletickets.models.TipoDeEspetaculo.SHOW;
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
 		TipoDeEspetaculo categoriaDoEspetaculo = sessao.getEspetaculo().getTipo();
+		
 		int ingressosRestantes = sessao.getTotalIngressos() - sessao.getIngressosReservados();
 		double totalDeIngressos = sessao.getTotalIngressos().doubleValue();
-		double taxaDeIngresso;
+
+		BigDecimal preco;
+		
+		double taxaCinemaShow = 0.10;
+		double taxaBalletOrquestra = 0.20;
 		
 		if(categoriaDoEspetaculo.equals(CINEMA) || categoriaDoEspetaculo.equals(SHOW)) {
 			//quando estiver acabando os ingressos...
-			taxaDeIngresso = 0.10;
 			
 			if(ingressosRestantes / totalDeIngressos <= 0.05) {
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxaDeIngresso)));
+				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxaCinemaShow)));
 			} else {
 				preco = sessao.getPreco();
 			}
 		} else if(categoriaDoEspetaculo.equals(BALLET) || categoriaDoEspetaculo.equals(ORQUESTRA)) {
-			taxaDeIngresso = 0.20;
 			
 			if(ingressosRestantes / totalDeIngressos <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxaDeIngresso)));
+				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxaBalletOrquestra)));
 			} else {
 				preco = sessao.getPreco();
 			}
