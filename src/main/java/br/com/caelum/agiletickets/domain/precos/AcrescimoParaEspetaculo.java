250e711e7cb89782203658d3d2f78a6fb93af35b
Luis <lgaa2006@gmail.com>
2015-04-16 12:55:45 -0300

    Removing staticness of methods

--- src/main/java/br/com/caelum/agiletickets/domain/precos/AcrescimoParaEspetaculo.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/AcrescimoParaEspetaculo.java
@@ -7,7 +7,7 @@ import br.com.caelum.agiletickets.models.Sessao;
 
 public class AcrescimoParaEspetaculo {
 
-	static BigDecimal atualizaPreco(Sessao sessao, double taxa) {
+	public BigDecimal atualizaPreco(Sessao sessao, double taxa) {
 		return sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(taxa)));
 	}
 
