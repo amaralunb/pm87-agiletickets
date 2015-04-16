1a0c6b55d9451d2c4ba628af6b4114526bd95a60
Luis <lgaa2006@gmail.com>
2015-04-16 09:47:17 -0300

    Refatoração: extract constant

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -7,19 +7,25 @@ import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
 public class CalculadoraDePrecos {
 
+	private static final TipoDeEspetaculo ORQUESTRA = TipoDeEspetaculo.ORQUESTRA;
+	private static final TipoDeEspetaculo BALLET = TipoDeEspetaculo.BALLET;
+	private static final TipoDeEspetaculo SHOW = TipoDeEspetaculo.SHOW;
+	private static final TipoDeEspetaculo CINEMA = TipoDeEspetaculo.CINEMA;
+	private static final TipoDeEspetaculo TEATRO = TipoDeEspetaculo.TEATRO;
+
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
 		TipoDeEspetaculo categoriaDoEspetaculo = sessao.getEspetaculo().getTipo();
 		int ingressosRestantes = sessao.getTotalIngressos() - sessao.getIngressosReservados();
 		
-		if(categoriaDoEspetaculo.equals(TipoDeEspetaculo.CINEMA) || categoriaDoEspetaculo.equals(TipoDeEspetaculo.SHOW)) {
+		if(categoriaDoEspetaculo.equals(CINEMA) || categoriaDoEspetaculo.equals(SHOW)) {
 			//quando estiver acabando os ingressos... 
 			if(ingressosRestantes / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			} else {
 				preco = sessao.getPreco();
 			}
-		} else if(categoriaDoEspetaculo.equals(TipoDeEspetaculo.BALLET)) {
+		} else if(categoriaDoEspetaculo.equals(BALLET)) {
 			if(ingressosRestantes / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			} else {
@@ -29,7 +35,7 @@ public class CalculadoraDePrecos {
 			if(sessao.getDuracaoEmMinutos() > 60){
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		} else if(categoriaDoEspetaculo.equals(TipoDeEspetaculo.ORQUESTRA)) {
+		} else if(categoriaDoEspetaculo.equals(ORQUESTRA)) {
 			if(ingressosRestantes / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			} else {
